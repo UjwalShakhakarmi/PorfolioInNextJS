f62c97ad92335f7c31b16cd4e07d93b95a7aaf70
UjwalShakhakarmi <ujwalshakha@gmail.com>
2024-09-21 22:37:19 +0545

    Almost Completed

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,10 +6,11 @@ import { navItems } from "../../data";
 import Clients from "./components/Clients";
 import Experience from "./components/Experience";
 import { Approach } from "./components/Approach";
+import Footer from "./components/footer";
 
 export default function Home() {
   return (
-    <main className="relative bg-black-100 flex justify-center items-center flex-col overflow-hidden mx-auto sm:px-10 px-5">
+    <main className="relative bg-black-100 flex justify-center items-center flex-col overflow-clip mx-auto sm:px-10 px-5">
       <div className="max-w-7xl w-full">
         <FloatingNav navItems={navItems} />
         <Hero />
@@ -18,6 +19,7 @@ export default function Home() {
         <Clients />
         <Experience />
         <Approach />
+        <Footer />
       </div>
     </main>
   );
