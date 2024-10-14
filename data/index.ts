5c2a621d0ca0394fec94756c2ff1dc610ae02da3
UjwalShakhakarmi <ujwalshakha@gmail.com>
2024-10-14 21:57:31 +0545

    new

--- data/index.ts
+++ data/index.ts
@@ -180,22 +180,22 @@ export const workExperience = [
   },
   {
     id: 2,
-    title: "Mobile App Dev - JSM Tech",
-    desc: "Designed and developed mobile app for both iOS & Android platforms using React Native.",
+    title: "Backend Developer Intern",
+    desc: "Assisted in the development of a web-based platform using Laravel, enhancing interactivity.",
     className: "md:col-span-2", // change to md:col-span-2
     thumbnail: "/exp2.svg",
   },
   {
     id: 3,
-    title: "Freelance App Dev Project",
-    desc: "Led the dev of a mobile app for a client, from initial concept to deployment on app stores.",
+    title: "Freelance Web Dev Project",
+    desc: "Collaboratively developed and maintained a scalable E-commerce platform, implementing features to enhance user experience and optimize performance.",
     className: "md:col-span-2", // change to md:col-span-2
     thumbnail: "/exp3.svg",
   },
   {
     id: 4,
-    title: "Lead Frontend Developer",
-    desc: "Developed and maintained user-facing features using modern frontend technologies.",
+    title: "Lead Frontend and Backend Developer",
+    desc: "Designed and implemented user-facing features and backend functionality for a JobPortal website as part of a college project.",
     className: "md:col-span-2",
     thumbnail: "/exp4.svg",
   },
--- src/app/components/newRecentProjects.tsx
+++ src/app/components/newRecentProjects.tsx
@@ -15,10 +15,7 @@ export default function RecentProjects() {
             className="sm:h-[41rem] h-[32rem] lg:min-h-[32.5rem] flex items-center justify-center sm:w-[570px] w-[80vw]"
             key={item.id}
           >
-            <PinContainer
-              title="/ui.aceternity.com"
-              href="https://twitter.com/mannupaaji"
-            >
+            <PinContainer title={item.title} href={item.link}>
               <div className="relative flex items-center justify-center sm:w-[570px] w-[80vw] overflow-hidden sm:h-[40vh] h-[30vh]  mb-10">
                 <div
                   className="relative w-full h-full overflow-hidden lg:rounded-3xl"
@@ -66,7 +63,11 @@ export default function RecentProjects() {
                   <p className="flex lg:text-xl md:text-xs text-sm text-purple">
                     Check Live Site
                   </p>
-                  <FaLocationArrow className="ms-3" color="#CBACF9" />
+                  <FaLocationArrow
+                    className="ms-3"
+                    color="#CBACF9"
+                    onClick={projects.link}
+                  />
                 </div>
               </div>
             </PinContainer>
